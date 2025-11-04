960197a3834225d808fbb340782ade298bcc52e5
aditya6386 <adityapatel1943524@gmail.com>
2025-11-04 11:27:04 +0530

    Updated Ui and logo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.ai_assistant"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
